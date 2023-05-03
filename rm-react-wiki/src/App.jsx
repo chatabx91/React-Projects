6a4c0a7ac1d61925d8ce6b433a1629cbea820b05
Jaspreet <chatabx91@gmail.com>
2023-05-03 12:30:45 -0500

    Utilized useEffect to fetch api data and setup state variable with useState.

--- rm-react-wiki/src/App.jsx
+++ rm-react-wiki/src/App.jsx
@@ -1,15 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import './App.css';
 import { useState, useEffect } from "react";
 // import Search from "./components/search/Search"
 // import Card from "./components/card/Card";
 // import Pagination from "./components/pagination/Pagination";
 // import Filter from "./components/filter/Filter";
 // import Navbar from "./components/navbar/Navbar";
-import './App.css'
 
+// Parent component
 function App() {
-  const [count, setCount] = useState(0)
+  // State variable
+  const [fetchData, setFetchData] = useState([]);
+  
+  // Destructure keys from fetchData
+  const {info, results} = fetchData;
+
+  // API endpoint
+  const endpoint = `https://rickandmortyapi.com/api/character/?page=1`;
+
+  // Execute function on render once
+  useEffect(()=> { 
+    // API fetch, update state with converted JSON data
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => setFetchData(data))
+      .catch(err => console.log(err.message))
+  },[]);
 
   return (
     <div className="App">
